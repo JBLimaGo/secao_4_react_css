1705dda02a877929550ef14bd02106ba1faf0038
Jefferson Lima <jefferson@araguaiasistemas.com.br>
2024-09-09 21:43:08 -0300

    CSS Componente - CSS utilizando com componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import MyComponent from "./components/MyComponent";
 
 function App() {
   return (
     <div className="App">
+      {/* CSS Global */}
       <h1>React Com CSS</h1>
+      {/* CSS Componente - CSS utilizando com componentes */}
+      <MyComponent />
+      <p>Este paragrafo e do App.js</p>
     </div>
   );
 }
---
+++ src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import "./MyComponent.css";
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>CSS Compomente</h1>
+      <p>Este é o paragrafo do componente</p>
+      <p className="my-comp-paragraph"> Este tb é do componente.</p>
+    </div>
+  );
+}
+
+export default MyComponent;
