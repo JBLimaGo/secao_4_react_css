abb8275fce07abdd36680c842c5209afffcf909f
Jefferson Lima <jefferson@araguaiasistemas.com.br>
2024-09-09 21:59:16 -0300

    Inline CSS - Não se usa muito pois se tiver que criar tera que dar manutenção em cada linha onde estiver.       O correto e usar Componente pois posso padronizar para todos lugares onde estiver usando

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ function App() {
       {/* CSS Componente - CSS utilizando com componentes */}
       <MyComponent />
       <p>Este paragrafo e do App.js</p>
+
+      {/* Inline CSS - Não se usa muito pois se tiver que criar tera que dar manutenção em cada linha onde estiver. 
+          O correto e usar Componente pois posso padronizar para todos lugares onde estiver usando*/}
+
+      <p style={{ color: "blue", padding: "25px", borderTop: "2px solid red" }}>
+        Este Elemento foi estilazado de forma inline
+      </p>
     </div>
   );
 }
