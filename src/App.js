6078185cbc974d97eb9557705813e9aad6fa8665
Jefferson Lima <jefferson@araguaiasistemas.com.br>
2024-09-10 21:23:04 -0300

    CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import MyComponent from "./components/MyComponent";
+import Title from "./components/Title";
 
 function App() {
   const n = 15;
@@ -44,6 +45,9 @@ function App() {
       <h2 className={redTitle ? "red-title" : "title"}>
         Este título vai ter classe dinâmica
       </h2>
+
+      {/* CSS Modules */}
+      <Title />
     </div>
   );
 }
